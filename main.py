4326343c6fb83becd57f21d14fce4cd1bbebb86e
Ali Alashawal <aliashwal03@gmail.com>
2025-08-26 16:01:40 +0300

    done

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 
 from fastapi import FastAPI
 from controllers.teas import router as TeasRouter  # Import the teas router
+from controllers.comments import router as CommentsRouter 
 
 app = FastAPI()
 
 # Include the teas router with a prefix '/api'
 app.include_router(TeasRouter, prefix="/api")
 
+app.include_router(CommentsRouter, prefix="/api")
 
 @app.get('/')
 def home():
--- serializers/comment.py
+++ serializers/comment.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional, List
+
 
 class CommentSchema(BaseModel):
-  id: int
+  id: Optional[int] = True
   content: str
 
   class Config:
